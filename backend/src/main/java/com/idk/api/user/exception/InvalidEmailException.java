afc303fe7d4098c335e3f6a7cd932bdd9a797854
minhaeun <haeunee6575@gmail.com>
2022-09-21 02:25:09 +0900

    style: 오타 수정 (#8)

--- backend/src/main/java/com/idk/api/user/exception/InvalidEmailException.java
+++ backend/src/main/java/com/idk/api/user/exception/InvalidEmailException.java
@@ -2,6 +2,6 @@ package com.idk.api.user.exception;
 
 public class InvalidEmailException extends IllegalStateException{
     public InvalidEmailException() {
-        super("등록된 사용자 정보가 없습니.");
+        super("등록된 사용자 정보가 없습니다.");
     }
 }
