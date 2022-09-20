347a34fd65d3eac1b6e62b180c6b02b2f4bbbf61
최지원 <weejee0605@gmail.com>
2022-09-20 13:30:05 +0900

    feat: header 구현(#12)

---
+++ frontend/src/views/common/HeaderView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <img src="../../assets/icon/header.png" alt="" id="home" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  width: 390px;
+  height: 73px;
+  /* color: black; */
+  /* background-color: skyblue; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.header > * {
+  padding-left: 55px;
+}
+</style>
--- frontend/src/views/home/HomeView.vue
+++ frontend/src/views/home/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <div class="home">
+    <div class="background">
       <div class="body">
+        <header-view class="header" />
         <footer-view class="footer" />
       </div>
     </div>
@@ -10,24 +11,36 @@
 
 <script>
 import FooterView from "../common/FooterView.vue";
+import HeaderView from "../common/HeaderView.vue";
 export default {
   components: {
     FooterView,
+    HeaderView,
   },
 };
 </script>
 
 <style scoped>
-.home {
+.background {
   display: flex;
   justify-content: center;
 }
+
 .body {
+  background-color: #f2f2f7;
   width: 390px;
   height: 844px;
   /* width: 100vw; */
   /* height: 100vh; */
   left: 100;
-  background-color: black;
+  /* background-color: black; */
+}
+.header {
+  position: fixed;
+  top: 0;
+}
+.footer {
+  position: fixed;
+  bottom: 0;
 }
 </style>
