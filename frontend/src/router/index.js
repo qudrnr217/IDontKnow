08b713a750ddc9e44ff4c8b8103796984fda4c4d
qudrnr217 <dksk1233@naver.com>
2022-09-20 22:42:41 +0900

    feat: 초기 폴더 생성 (#13)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/home/HomeView.vue";
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -7,18 +10,20 @@ const routes = [
     name: "home",
     component: HomeView,
   },
-  // {
-  //   path: "/about",
-  //   name: "about",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  // },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
 ];
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes,
 });
 
--- frontend/src/views/community/CommunityView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>community</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
