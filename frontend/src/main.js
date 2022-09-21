0bb2c77213b695c43959a1d0670221190367dcd0
qudrnr217 <dksk1233@naver.com>
2022-09-21 17:18:32 +0900

    feat: 테스트용 (#26)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import Carousel3d from "vue-carousel-3d";
 Vue.config.productionTip = false;
+
+Vue.use(Carousel3d);
 
 new Vue({
   router,
