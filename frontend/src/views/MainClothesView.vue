6765a5309b5438a409c6a7d4fe3ddb4a92f3effd
최지원 <weejee0605@gmail.com>
2022-09-26 17:36:10 +0900

    feat: 메인페이지 옷추천페이지 레이아웃 구현 (#44)

--- frontend/src/views/MainClothesView.vue
+++ frontend/src/views/MainClothesView.vue
@@ -2,29 +2,31 @@
   <div class="background">
     <div class="body">
       <header-view class="header" />
-      <dropdown-menu />
-      <dropdown-location />
-      <div class="clothes">
-        <div class="content_title">의상 추천</div>
+      <div class="content-box3">
+        <div class="content">
+          <div class="content_title">오늘의</div>
+          <div class="content_dropdown"><dropdown-menu /></div>
+        </div>
       </div>
-      <div class="weather">
-        <table>
-          <tr>
-            <th><img /></th>
-            <th>기온</th>
-            <th><img /></th>
-            <th>바람세기</th>
-          </tr>
-          <tr>
-            <th><img /></th>
-            <th>습도</th>
-            <th><img /></th>
-            <th>강수량</th>
-          </tr>
-          <tr>
-            <th><div class="weather_comment">오늘은 덥습니다.</div></th>
-          </tr>
-        </table>
+      <div class="current-location"><dropdown-location /></div>
+
+      <div class="content-box3">
+        <div class="content" style="flex-direction: column">
+          <div class="content-title">오늘의 의상 추천</div>
+          <div class="content-img">
+            <img
+              src="../assets/image/clothes/12~16.png"
+              alt
+              class="clothes_img"
+            />
+          </div>
+          <div class="content-description">
+            얇은 니트, 맨투맨, 가디건, 청바지 가 좋을 것 같네요!
+          </div>
+        </div>
+      </div>
+      <div class="content-box3">
+        <weather-view />
       </div>
       <footer-view class="footer" />
     </div>
@@ -36,40 +38,52 @@ import HeaderView from "../components/common/HeaderView.vue";
 import FooterView from "../components/common/FooterView.vue";
 import DropdownMenu from "../components/main/DropdownMenu.vue";
 import DropdownLocation from "@/components/regist/DropdownLocation.vue";
+import WeatherView from "../components/main/WeatherView.vue";
 export default {
   components: {
     HeaderView,
     FooterView,
     DropdownMenu,
     DropdownLocation,
+    WeatherView,
   },
 };
 </script>
 
-<style>
-.clothes {
-  width: 300px;
-  height: 300px;
-  background-color: #ffffff;
-  border: none;
-  border-radius: 6px;
-}
-.content_title {
+<style scoped>
+.content-box3 {
+  display: flex;
   justify-content: center;
-  flex-grow: 1;
+  align-content: center;
+  width: 100%;
+  font-family: "GmarketSansTTFLight";
+  font-style: normal;
+}
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  width: 250px;
+  border: none;
+  border-radius: 10px;
+  margin: 10px;
+  padding: 10px;
+  font-family: "GmarketSansTTFLight";
+}
+.content-title {
+  display: center;
+  justify-content: center;
+  flex-grow: 2;
   font-size: 15px;
   font-family: "GmarketSansTTFMedium";
   vertical-align: center;
+  margin: 10px;
 }
-.weather {
-  width: 300px;
-  height: 300px;
-  background-color: #ffffff;
-  border: none;
-  border-radius: 6px;
+.content-img {
+  margin: 10px;
 }
-/* .weather_comment {
-  width: 300px;
-  height: 100px;
-} */
+.content-description {
+  margin: 10px;
+}
 </style>
