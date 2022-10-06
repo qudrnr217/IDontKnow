8b28f3e3614768e5c965a4a2bd8349237ce4715b
강병국 <dksk1233@naver.com>
2022-10-06 13:00:45 +0900

    Revert "Merge branch 'feat/91/community/image' into 'fe'"
    This reverts merge request !99

--- frontend/src/components/home/VoteList.vue
+++ frontend/src/components/home/VoteList.vue
@@ -92,6 +92,7 @@
           </option>
         </select>
       </div>
+
       <div>
         <div class="vote-list">
           <!-- v-for="vote in voteList" :key="vote.voteId"  -->
@@ -101,14 +102,6 @@
             :key="index"
             @click="detailCard"
             :value="`${vote.voteId}`"
-            :style="{
-              backgroundImage: `linear-gradient(
-                rgba(255, 255, 255, 0.5),
-                rgba(255, 255, 255, 0.5)
-                ), url(${require('@/assets/image/category/menu/' +
-                  vote.subCategory.replace('/', '_') +
-                  '.jpg')})`,
-            }"
           >
             <div
               class="vote-title-box"
